13770df815df6b7d6371c49353b56124cc2e6c3a
aaronRhodebeck <arhodebeck@gmail.com>
2017-02-22 19:52:19 -0600

    Twitch api call handled

---
+++ script.js
@@ -0,0 +1,35 @@
+function TwitchUser(userName) {
+    var thisUser = this;
+    this.userName = userName;
+    this.channelURL;
+    this.isCurrentlyStreaming;
+    this.currentStreamInfo;
+
+    createChannelURL(userName);
+
+    function createChannelURL(userName) {
+        thisUser.channelURL = "https://www.twitch.tv/" + userName;
+    }
+
+    this.getCurrentTwitchStreamInfo = function() {
+        var twitchAPIUrl = "https://wind-bow.gomix.me/twitch-api/streams/";
+        var apiCall = twitchAPIUrl + thisUser.userName;
+        var promise = new Promise(function(resolve, reject) {
+            $.getJSON(apiCall).then(function(json) {
+                thisUser.currentStreamInfo = json;
+                thisUser.isCurrentlyStreaming = setIsCurrentlyStreaming(json);
+                resolve();
+            });
+        });
+        return promise;
+
+        function setIsCurrentlyStreaming(streamInfo) {
+            if (streamInfo.stream) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
